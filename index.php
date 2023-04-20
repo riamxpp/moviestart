16bee8f24c7e072baf97f9ad07a7ef767385140e
riamxpp <riamstefeson616@gmail.com>
2023-04-20 15:53:45 -0300

    Criando header e footer, já criando a estilização de ambos

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
   require_once("globals.php");
   require_once("models/db.php");
-    
+
 ?>
 
 <!DOCTYPE html>
@@ -14,17 +14,63 @@
   <title>MovieStart</title>
   <!-- FONT AWESOME -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="shortcut icon" href="img/moviestar.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="<?= $BASE_URL ?>img/moviestar.ico" type="image/x-icon">
   <!-- BOOTSTRAP -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.rtl.css" crossorigin="anonymous">
   <!-- CSS -->
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
 </head>
 <body>
-  <h1>Testando movie star</h1>
-  <p>
-    <i class="far fa-plus-square"></i>
-  </p>
+  <header>
+    <nav id="main-navbar" class="navbar navbar-expand-lg">
+      <a href="<?= $BASE_URL ?>index.php" class="navbar-brand">
+        <img src="<?= $BASE_URL ?>img/logo.svg" alt="MovieStar" id="logo">
+        <span id="moviestar-title">MovieStar</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+      <form action="" method="GET" class="form-inline my-2 my-lg-0" id="search-form">
+        <input type="search" name="q" id="search" class="form-control mr-sm-2" placeholder="Buscar filmes" aria-label="Search">
+        <button class="btn my-2 my-sm-0" type="submit">
+          <i class="fas fa-search"></i>
+        </button>
+      </form>
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <div id="main-container" class="container-fluid">
+    <h1>Corpo do site</h1>
+  </div>
+  <footer id="footer">
+    <div id="social-container">
+      <ul>
+        <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+        <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+      </ul>
+    </div>
+    <div id="footer-links-container">
+      <ul>
+        <li>
+          <a href="">Adicionar filme</a>
+        </li>
+        <li>
+          <a href="">Adicionar crítica</a>
+        </li>
+        <li>
+          <a href="">Entrar / Registrar</a>
+        </li>
+      </ul>
+    </div>
+    <p>&copy; Riam Stefeson</p>
+  </footer>
   <!-- BOOTSTRAP JS -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.js" integrity="sha512-L6XANV6sOsx9N9c787eDN1pjB2Pzautd3xDgn4cMKuoleHSuCJi5pCDGPCtwE3Bd4A1Olnr0k0aQXbczYzg+wg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
