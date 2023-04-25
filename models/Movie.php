31f42aa77d22a26eaee6f3e80a2d9e85d88d9588
riamxpp <riamstefeson616@gmail.com>
2023-04-25 15:20:48 -0300

    Finalizando css do arquivo 'newmovie.php' e criando a classe Movie e a InterfaceMovie

---
+++ models/Movie.php
@@ -0,0 +1,37 @@
+<?php
+
+  class Movie {
+
+    public $id;
+    public $title;
+    public $description;
+    public $img;
+    public $trailer;
+    
+    public $category;
+    
+    public $length;
+    
+    public $users_id;
+
+    // Duplicando código pq só vou repetir esta função, mas o correto seria criar um model para img;
+    public function imageGenerateName(){
+      return bin2hex(random_bytes(60)) . ".jpg";
+    }
+
+  }
+
+  interface MovieInterface {
+
+    public function buildMovie($data);
+    public function findAll();
+    public function getLatesMovies();
+    public function getMovieByCategory($category);
+    public function getMovieByUserId($id);
+    public function findById($id);
+    public function findByTitle($title);
+    public function create(Movie $movie);
+    public function update(Movie $movie);
+    public function destroy($id);
+
+  }
