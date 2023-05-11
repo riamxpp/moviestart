23126a5c1af980e000eb050b121426bd11eab29c
riamxpp <riamstefeson616@gmail.com>
2023-05-11 09:55:14 -0300

    Inserindo imagem do usuário no seu comentário

--- templates/Review.php
+++ templates/Review.php
@@ -1,8 +1,7 @@
 <div class="col-md-12 review">
   <div class="row">
     <div class="col-md-2">
-      <div class="profile-image-container review-image" style="background-image: url('<?= $BASE_URL
-        ?>img/users/user.png')"></div>
+      <div class="profile-image-container review-image" style="background-image: url('<?= $review->img_user ?>')"></div>
     </div>
     <div class="col-md-9 author-details-container">
       <h4 class="author-name">
